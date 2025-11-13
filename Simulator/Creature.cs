937778b141cf7b0f722efcf7234c404f77e968c5
Domel <239909@student.uek.krakow.pl>
2025-11-13 19:16:49 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        set => level = value > 0 ? value : 1;
+    }
+
+    public Creature(string name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, level {Level} creature.");
+    }
+
+    public string Info()
+    {
+        return $"{Name} [{Level}]";
+    }
+}
---
+++ Simulator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Simulator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Simulator!\n");
+
+        var dogs = new Animals
+        {
+            Description = "Dogs",
+            Size = 3
+        };
+
+        Console.WriteLine(dogs.Info);
+    }
+}
