aad3afcfe61fc5cbaaa5000d2e0ed52351b36b23
Domel <239909@student.uek.krakow.pl>
2025-12-08 16:11:49 +0100

    Mapy

---
+++ Simulator/Maps/Map.cs
@@ -0,0 +1,8 @@
+﻿namespace Simulator.Maps;
+
+public abstract class Map
+{
+    public abstract bool Exist(Point p);
+    public abstract Point Next(Point p, Direction d);
+    public abstract Point NextDiagonal(Point p, Direction d);
+}
---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,34 @@
+﻿using Simulator;
+
+namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    public int Size { get; }
+
+    private readonly Rectangle rect;
+
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+            throw new ArgumentOutOfRangeException(nameof(size));
+
+        Size = size;
+        rect = new Rectangle(0, 0, size - 1, size - 1);
+    }
+
+    public override bool Exist(Point p)
+        => rect.Contains(p);
+
+    public override Point Next(Point p, Direction d)
+    {
+        var np = p.Next(d);
+        return Exist(np) ? np : p;
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        var np = p.NextDiagonal(d);
+        return Exist(np) ? np : p;
+    }
+}
